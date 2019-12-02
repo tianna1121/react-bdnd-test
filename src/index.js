3cbff6c6eb690b8db11c17a46a3aed72186f7fdc
yang zhang <tianna1121@gmail.com>
2019-12-02 14:20:57 +0800

    fixed the innerRef issue. ref: https://github.com/atlassian/react-beautiful-dnd/issues/924

--- src/index.js
+++ src/index.js
@@ -41,93 +41,93 @@ const move = (source, destination, droppableSource, droppableDestination) => {
 const grid = 8;
 
 const Content = styled.div`
-  margin-right: 200px;
+    margin-right: 200px;
 `;
 
 const Item = styled.div`
-  display: flex;
-  user-select: none;
-  padding: 0.5rem;
-  margin: 0 0  0.5rem 0;
-  align-items: flex-start;
-  align-content: flex-start;
-  line-height: 1.5;
-  border-radius: 3px;
-  background: #fff;
-  border: 1px ${props => (props.isDragging ? 'dashed #000' : 'solid #ddd')};
-
- `;
+    display: flex;
+    user-select: none;
+    padding: 0.5rem;
+    margin: 0 0 0.5rem 0;
+    align-items: flex-start;
+    align-content: flex-start;
+    line-height: 1.5;
+    border-radius: 3px;
+    background: #fff;
+    border: 1px ${props => (props.isDragging ? 'dashed #000' : 'solid #ddd')};
+`;
 
 const Clone = styled(Item)`
-  + div {
-    display: none!important;
-  }
+    + div {
+        display: none !important;
+    }
 `;
 
 const Handle = styled.div`
-  display: flex;
-  align-items: center;
-  align-content: center;
-  user-select: none;
-  margin: -0.5rem 0.5rem -0.5rem -0.5rem;
-  padding: 0.5rem;
-  line-height: 1.5;
-  border-radius: 3px 0 0 3px;
-  background: #fff;
-  border-right: 1px solid #ddd;
-  color: #000;
+    display: flex;
+    align-items: center;
+    align-content: center;
+    user-select: none;
+    margin: -0.5rem 0.5rem -0.5rem -0.5rem;
+    padding: 0.5rem;
+    line-height: 1.5;
+    border-radius: 3px 0 0 3px;
+    background: #fff;
+    border-right: 1px solid #ddd;
+    color: #000;
 `;
 
 const List = styled.div`
-  border: 1px ${props => (props.isDraggingOver ? 'dashed #000' : 'solid #ddd')};
-  background: #fff;
-  padding: 0.5rem 0.5rem 0;
-  border-radius: 3px;
-  flex: 0 0 150px;
-  font-family: sans-serif;
+    border: 1px
+        ${props => (props.isDraggingOver ? 'dashed #000' : 'solid #ddd')};
+    background: #fff;
+    padding: 0.5rem 0.5rem 0;
+    border-radius: 3px;
+    flex: 0 0 150px;
+    font-family: sans-serif;
 `;
 
 const Kiosk = styled(List)`
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  width: 200px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    width: 200px;
 `;
 
 const Container = styled(List)`
-  margin: 0.5rem 0.5rem 1.5rem;
+    margin: 0.5rem 0.5rem 1.5rem;
 `;
 
 const Notice = styled.div`
-  display: flex;
-  align-items: center;
-  align-content: center;
-  justify-content: center;
-  padding: 0.5rem;
-  margin: 0 0.5rem 0.5rem;
-  border: 1px solid transparent;
-  line-height: 1.5;
-  color: #aaa;
+    display: flex;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+    padding: 0.5rem;
+    margin: 0 0.5rem 0.5rem;
+    border: 1px solid transparent;
+    line-height: 1.5;
+    color: #aaa;
 `;
 
 const Button = styled.button`
-  display: flex;
-  align-items: center;
-  align-content: center;
-  justify-content: center;
-  margin: 0.5rem;
-  padding: 0.5rem;
-  color: #000;
-  border: 1px solid #ddd;
-  background: #fff;
-  border-radius: 3px;
-  font-size: 1rem;
-  cursor: pointer;
+    display: flex;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+    margin: 0.5rem;
+    padding: 0.5rem;
+    color: #000;
+    border: 1px solid #ddd;
+    background: #fff;
+    border-radius: 3px;
+    font-size: 1rem;
+    cursor: pointer;
 `;
 
 const ButtonText = styled.div`
-  margin: 0 1rem;
+    margin: 0 1rem;
 `;
 
 const ITEMS = [
@@ -210,7 +210,7 @@ class App extends Component {
                 <Droppable droppableId="ITEMS" isDropDisabled={true}>
                     {(provided, snapshot) => (
                         <Kiosk
-                            innerRef={provided.innerRef}
+                            ref={provided.innerRef}
                             isDraggingOver={snapshot.isDraggingOver}>
                             {ITEMS.map((item, index) => (
                                 <Draggable
@@ -220,7 +220,7 @@ class App extends Component {
                                     {(provided, snapshot) => (
                                         <React.Fragment>
                                             <Item
-                                                innerRef={provided.innerRef}
+                                                ref={provided.innerRef}
                                                 {...provided.draggableProps}
                                                 {...provided.dragHandleProps}
                                                 isDragging={snapshot.isDragging}
@@ -254,7 +254,7 @@ class App extends Component {
                         <Droppable key={list} droppableId={list}>
                             {(provided, snapshot) => (
                                 <Container
-                                    innerRef={provided.innerRef}
+                                    ref={provided.innerRef}
                                     isDraggingOver={snapshot.isDraggingOver}>
                                     {this.state[list].length
                                         ? this.state[list].map(
@@ -265,7 +265,7 @@ class App extends Component {
                                                       index={index}>
                                                       {(provided, snapshot) => (
                                                           <Item
-                                                              innerRef={
+                                                              ref={
                                                                   provided.innerRef
                                                               }
                                                               {...provided.draggableProps}
